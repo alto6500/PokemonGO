ae30065abc6a6686b9e598b14a139c56ffe30930
alto6500 <146078331+alto6500@users.noreply.github.com>
2024-04-26 11:28:15 +0200

    Add files via upload

---
+++ PokemonGO/src/pokemongo/PokemonGO.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pokemongo;
+
+/**
+ *
+ * @author alto6500
+ */
+public class PokemonGO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PokemonGO app = new PokemonGO();
+        app.run();
+        
+    }
+
+    private void run() {
+        /*mostrar caratula*/
+        /*recuperar datos fichero de caratula*/
+        /*mostrar por pantalla caratula*/
+        /*recorrer ArraylistString y mostrarlo por pantalla*/
+        
+        
+        /*mostrar el menu*/
+        /*con o sale con las demas poner frase..."has elegido la opcion.."*/
+        
+        /*De la fase 2 hace modelo entitat-relació y model relacional*/
+
+    }
+    
+}
